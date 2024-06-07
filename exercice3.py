2bda7cbb28b2c64315be675721ed6e3a3d978884
appoliaireAlitcha <Appolinaire@alitchateam.com>
2024-06-07 17:00:30 +0100

    feat: exercices 4, 5, 6, 7

--- exercice3.py
+++ exercice3.py
@@ -2,8 +2,8 @@ def isPalindrome(t):
     if len(t) <= 1:
         return True
     
-    if t[0] == t[-1]:
-        return isPalindrome(t[1:-1])
+    if t[0] == t[-1] and  isPalindrome(t[1:-1]):
+        return True
 
     return False
    
